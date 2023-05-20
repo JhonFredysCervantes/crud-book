c8bda03143a28e698a4faf6f0f0cf2719947d232
Cervantes Jhon <jhonfredycercha@hotmail.com>
2023-05-20 08:50:25 -0500

    chore(*): adding firebase to repository

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbD95CvkABcw5Asnv7lrqx4KvknCdmgIM",
+  authDomain: "spe-unimag.firebaseapp.com",
+  databaseURL: "https://spe-unimag-default-rtdb.firebaseio.com",
+  projectId: "spe-unimag",
+  storageBucket: "spe-unimag.appspot.com",
+  messagingSenderId: "88178932667",
+  appId: "1:88178932667:web:23ccfbfef1e988f267229e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFireStore(app);
+export { db };
